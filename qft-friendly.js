d2634a34fe4d34267f2a80cd57246497d7792bcc
Joseph Reed <josephreed2600@gmail.com>
2018-03-09 11:44:01 -0800

    add s, remove w
    see https://codegolf.stackexchange.com/questions/11880/build-a-working-game-of-tetris-in-conways-game-of-life

--- qft-friendly.js
+++ qft-friendly.js
@@ -11,11 +11,10 @@ onkeypress = function(e) {
 	var i = 0;
 	switch(e.key) {
 		case 'q': i=1; break;
-		case 'e': i=16; break;
-		case 'w': i=0; break;
 		case 'a': i=2; break;
-		case 's': i=0; break;
+		case 's': i=4; break;
 		case 'd': i=8; break;
+		case 'e': i=16; break;
 		default: i=0; break;
 	}
 	RAMwrite(1,i);
